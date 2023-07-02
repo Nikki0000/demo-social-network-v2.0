f21c821a5eaf417d1aa26db677f2ab043f42baad
Nikki0000 <savodyarkin.n@mail.ru>
2023-07-02 15:25:53 +0300

    Updates

---
+++ precache-manifest.4d1701151c107b6fa0f9565aec9bda19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d2468a3af2f0155230f",
+    "url": "/demo-social-network-v2.0/static/css/main.08d730ec.chunk.css"
+  },
+  {
+    "revision": "9d2468a3af2f0155230f",
+    "url": "/demo-social-network-v2.0/static/js/main.9d2468a3.chunk.js"
+  },
+  {
+    "revision": "5545d045c0f52ce03c57",
+    "url": "/demo-social-network-v2.0/static/js/1.5545d045.chunk.js"
+  },
+  {
+    "revision": "328e50bb5bc8554c65ea",
+    "url": "/demo-social-network-v2.0/static/js/2.328e50bb.chunk.js"
+  },
+  {
+    "revision": "bd454e0332e39bbd4de6",
+    "url": "/demo-social-network-v2.0/static/js/runtime~main.bd454e03.js"
+  },
+  {
+    "revision": "cec7c2e432b3b3f97991657f903420b4",
+    "url": "/demo-social-network-v2.0/static/media/user.cec7c2e4.png"
+  },
+  {
+    "revision": "ae691311a5bce0220adbdeba35399fcc",
+    "url": "/demo-social-network-v2.0/static/media/Spinner-1s-200px.ae691311.svg"
+  },
+  {
+    "revision": "04b14c0c2087a67077b391d8cf34f205",
+    "url": "/demo-social-network-v2.0/index.html"
+  }
+];
